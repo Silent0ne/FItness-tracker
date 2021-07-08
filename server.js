dddf0b00c7c6352ec768043aeb7fb09336fa8c18
Kyle Nance <nancekyle15@gmail.com>
2021-07-08 15:10:32 -0400

    conneceted server created git ignore

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const http = require('http');
+const path = require('path');
 const fs = require('fs');
 const express = require('express');
+//const mongose = require('mongose');
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-require('./routes/htmlRoutes')(app);
+app.get
+//require('/routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
